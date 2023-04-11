346af82b75275455f1ee0107cae2d58f247d9803
Johnny Harbieh <jharbieh@microsoft.com>
2023-04-11 09:48:46 -0500

    added main cs operations

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 <!-- write Hello World in bolded text -->
 # Hello World
+
+
+## main.cs
+
+### Compile
+```
+."C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\Roslyn\csc.exe" .\main.cs
+```
+
+### Run
+```
+.\main.exe  
+ ```
+
+
--- main.cs
+++ main.cs
@@ -10,4 +10,36 @@ namespace HelloWorld
             Console.WriteLine("Hello World!");
         }
     }
+
+    class Speaker
+    {
+
+        public string Name { get; set; }
+        public string Language { get; set; }
+
+        public sbyte Age { get; set; }
+
+        public int SpeakerID { get; set; }
+
+        public Speaker(string name, string language, sbyte age)
+        {
+            Name = name;
+            Language = language;
+            Age = age;
+            SpeakerID = GetSpeakerID();
+        }
+
+        private int GetSpeakerID(){
+            // Generate random speaker ID from 1 to 1000
+            Random random = new Random();
+            int _speakerID = random.Next(1, 1000);
+
+            return _speakerID;
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
 }
