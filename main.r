4fad148babe74dfc7417ca14720e1a28fd1b9b74
Johnny Harbieh <jharbieh@microsoft.com>
2023-11-30 17:29:14 -0600

    updated r

--- main.r
+++ main.r
@@ -9,3 +9,13 @@ tail(df)
 
 # display the first 10 rows
 head(df, 10)
+
+# This is a single-line comment in R
+
+# You can also write comments at the end of a line of code, like this:
+print("Hello, world!") # This is a comment at the end of a line of code
+
+
+# Path: main.r
+
+
